dee0fbfda26691d182dddd209b9cd119fa8da4d4
Kyle Deen <kcd5059@gmail.com>
2019-01-20 21:35:51 -0500

    Starting from section 5 (midway) commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './modules/auth.module';
+import { AuthRoutingModule } from './modules/auth-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    AuthRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
